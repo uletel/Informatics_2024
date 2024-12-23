d03f54a7764b2a6f95220f30e71d7838c044d017
uletel <fleymoong@gmail.com>
2024-12-24 00:00:49 +0300

    Внёс правки по комментариям

--- golang/main.go
+++ golang/main.go
@@ -8,6 +8,5 @@ import (
 
 func main() {
 	fmt.Println("Губе Владислав Семёнович")
-	fmt.Println("Task A:\n", lab4.Task_A(3.2, 6.2, 0.6))
-	fmt.Println("Task B:\n", lab4.Task_B([]float64{4.48, 3.56, 2.78, 5.28, 3.21}))
+	lab4.RunLab4()
 }
